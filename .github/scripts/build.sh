2548ea22a7998754a26cd5600f837635bb84ec09
David Beckingsale <davidbeckingsale@gmail.com>
2023-12-13 15:09:05 -0800

    Updates

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -11,12 +11,12 @@ printf "Tag is ${tag}\n"
 container=$(basename $(dirname $basedir))          
 printf "Container is ${container}\n"
 cat ${result_name}
-container_name=ghcr.io/rse-ops/${container}:${tag}
+container_name=ghcr.io/llnl/radiuss-${container}:${tag}
 docker pull ${container_name} || echo "Container $container_name does not exist yet"
 printf "docker build -f ${dockerfile} -t ${container_name} .\n"
 docker build -f ${dockerfile} -t ${container_name} .
 
 echo "container_uri=${container_name}" >> $GITHUB_OUTPUT
-echo "uri=ghcr.io/rse-ops/${container}" >> $GITHUB_OUTPUT
+echo "uri=ghcr.io/llnl/radiuss-${container}" >> $GITHUB_OUTPUT
 echo "tag=${tag}" >> $GITHUB_OUTPUT
 echo "dockerfile_dir=${basedir}" >> $GITHUB_OUTPUT
